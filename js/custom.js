148449503c3a9390861192f592237f2667aeb2d2
Henri Susanto <henry.dinus@gmail.com>
2023-12-12 09:06:52 +0700

    lint up & fix carousel for prod

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-(function($) {
+(function ($) {
     var total_today = 0.00;
     var total_one_off = 0.00;
     var total_monthly = 0;
@@ -14,10 +14,10 @@
         var description = $(item).find("h2");
         var price = $(item).find(".product-price");
         var data_no = $(item).attr("data-no");
-        var input_text = "<div class='summary-item' data-id='"+product_id+"' data-no='"+data_no+"'><div class='remove-order'></div><div style='width: 100%;'><h1>"+title.html()+"</h1><h2>"+description.html()+"</h2></div><p>"+price.html()+"</p></div>";
+        var input_text = "<div class='summary-item' data-id='" + product_id + "' data-no='" + data_no + "'><div class='remove-order'></div><div style='width: 100%;'><h1>" + title.html() + "</h1><h2>" + description.html() + "</h2></div><p>" + price.html() + "</p></div>";
         $(".order-summary").append(input_text);
         var sd = price.text().match(/[0-9.]+/g);
-        
+
         total_today += parseFloat(sd);
         if (price.text().indexOf("mth") > 0) {
             total_monthly += parseFloat(sd);
@@ -29,7 +29,7 @@
 
     function removeProdct(item, product_id) {
         var summary = $(".summary-item");
-        for (var i = 0; i < summary.length; i ++) {
+        for (var i = 0; i < summary.length; i++) {
             var id = $(summary[i]).attr("data-id");
             if (product_id == id) {
                 $(summary[i]).remove();
@@ -39,7 +39,7 @@
 
         var price = $(item).find(".product-price");
         var sd = price.text().match(/[0-9.]+/g);
-        
+
         total_today -= parseFloat(sd);
         if (price.text().indexOf("mth") > 0) {
             total_monthly -= parseFloat(sd);
@@ -48,9 +48,9 @@
         }
         displayTotalPrice();
     }
-    
+
     function addToCart(product_id) {
-        $.get({url:AIRTEL_THE7_URL.SITE + '?post_type=product&add-to-cart=' + product_id, async:false}, function() {
+        $.get({ url: AIRTEL_THE7_URL.SITE + '?post_type=product&add-to-cart=' + product_id, async: false }, function () {
         });
     }
 
@@ -59,12 +59,12 @@
             type: "POST",
             url: AIRTEL_THE7_URL.SITE + 'wp-admin/admin-ajax.php',
             async: false,
-            data: {action : 'remove_item_from_cart', product_id : product_id},
+            data: { action: 'remove_item_from_cart', product_id: product_id },
             success: function (res) {
                 if (res) {
                 }
             }
-        });   
+        });
     }
 
     function loadProductsFromCart() {
@@ -72,40 +72,40 @@
             type: "POST",
             url: AIRTEL_THE7_URL.SITE + 'wp-admin/admin-ajax.php',
             async: false,
-            data: {action : 'load_item_from_cart'},
+            data: { action: 'load_item_from_cart' },
             success: function (res) {
                 if (res) {
                     var result = JSON.parse(res);
-                    
-                    for (var i = 0; i < result.ID.length; i ++) {
-                        var product = $("body").find(".product-item[data-id='"+result.ID[i]+"']");
+
+                    for (var i = 0; i < result.ID.length; i++) {
+                        var product = $("body").find(".product-item[data-id='" + result.ID[i] + "']");
                         $(product).addClass("selected");
                         $(product).find(".select-product-item").text("Selected");
                         addProduct(product, result.ID[i]);
                     }
                 }
             }
-        });   
+        });
     }
 
     function setCookie(cname, cvalue, exdays) {
         const d = new Date();
         d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-        let expires = "expires="+d.toUTCString();
+        let expires = "expires=" + d.toUTCString();
         document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
     }
-  
+
     function getCookie(cname) {
         let name = cname + "=";
         let ca = document.cookie.split(';');
-        for(let i = 0; i < ca.length; i++) {
-          let c = ca[i];
-          while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-          }
-          if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-          }
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
         }
         return "";
     }
@@ -113,65 +113,65 @@
     // Search Address
     var adresDB = new Bloodhound({
         datumTokenizer: function (datum) {
-        return Bloodhound.tokenizers.whitespace(datum.value);
-    },
-    queryTokenizer: Bloodhound.tokenizers.whitespace,
-    remote: {
-        //url: '/aus_addr.php/d?q=%QUERY',
-        url: '/aus_addr.php/d?address=%QUERY',
+            return Bloodhound.tokenizers.whitespace(datum.value);
+        },
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: {
+            //url: '/aus_addr.php/d?q=%QUERY',
+            url: '/aus_addr.php/d?address=%QUERY',
             filter: function (adresDB) {
                 console.log(adresDB);
                 return jQuery.map(adresDB.features, function (mm) {
-                    res =  {
-                        id:                     mm.properties.addressId,
-                        highlighted:            mm.properties.formattedAddress,
-                        address:                mm.properties.formattedAddress,
-                        flat_number:            mm.properties.complexUnitNumber,
-                        number_first:           mm.properties.streetNumber1,
-                        street_name:            mm.properties.streetName,
-                        street_type_code:       mm.properties.streetType,
-                        locality_name:          mm.properties.localityName,
-                        state_abbreviation:     mm.properties.stateTerritory,
-                        postcode:               mm.properties.postcode,
+                    res = {
+                        id: mm.properties.addressId,
+                        highlighted: mm.properties.formattedAddress,
+                        address: mm.properties.formattedAddress,
+                        flat_number: mm.properties.complexUnitNumber,
+                        number_first: mm.properties.streetNumber1,
+                        street_name: mm.properties.streetName,
+                        street_type_code: mm.properties.streetType,
+                        locality_name: mm.properties.localityName,
+                        state_abbreviation: mm.properties.stateTerritory,
+                        postcode: mm.properties.postcode,
                     };
                     console.log(res);
                     return res;
                 });
-              },
-        wildcard: '%QUERY'
-    }
+            },
+            wildcard: '%QUERY'
+        }
     });
-    
+
 
     // Initialize the Bloodhound suggestion engine
     adresDB.initialize();
-    
+
     // Instantiate the Typeahead UI
     jQuery('#location').typeahead(null, {
         name: 'make-model',
         highlight: true,
         displayKey: 'address',
         source: adresDB.ttAdapter(),
-                templates: {
-    	      notFound: [
-    	        '<div class="empty-message">',
-    	          '&nbsp;address not found! ',
-    	        '</div>'
-    	      ].join('\n'),
+        templates: {
+            notFound: [
+                '<div class="empty-message">',
+                '&nbsp;address not found! ',
+                '</div>'
+            ].join('\n'),
             suggestion: function (adresDB) {
                 return '<div>' + adresDB.highlighted + '</div>';
             }
-    
+
         }
     });
 
-	$('.close-search').click(function() {
-		$('#location').val('')
+    $('.close-search').click(function () {
+        $('#location').val('')
         $("#row_qualify_fail").addClass("hidden");
-        $("#row_qualify_success").addClass("hidden");		
-	});
-	
-	$('.search-location').on('typeahead:selected', function (e, datum) {
+        $("#row_qualify_success").addClass("hidden");
+    });
+
+    $('.search-location').on('typeahead:selected', function (e, datum) {
         var request = $.ajax({
             type: "POST",
             url: AIRTEL_THE7_URL.SERVICE + "api/find_service",
@@ -186,22 +186,22 @@
                 postcode: datum.postcode,
             }),
             dataType: "json",
-            headers: { 
+            headers: {
                 'Accept': 'application/json',
-                'Content-Type': 'application/json' 
-            },            
+                'Content-Type': 'application/json'
+            },
         });
 
         $("#row_qualify_fail").addClass("hidden");
         $("#row_qualify_success").addClass("hidden");
 
         $("#row_checking").removeClass("hidden");
-        
-		$.when(request).done(function(r1) {
-			console.log('AAPT:');
-			console.log(r1);
-			$("#row_checking").addClass("hidden");
-			data = r1;
+
+        $.when(request).done(function (r1) {
+            console.log('AAPT:');
+            console.log(r1);
+            $("#row_checking").addClass("hidden");
+            data = r1;
             if (data.serviceProviderLocationList !== undefined) {
                 var nbnLocationList = data.serviceProviderLocationList.filter(x => x.serviceProvider === 'NBN');
                 if (nbnLocationList.length > 0) {
@@ -214,31 +214,31 @@
                             type: "POST",
                             url: AIRTEL_THE7_URL.SERVICE + "api/qualify_service",
                             data: JSON.stringify({
-                              serviceProvider: "NBN",
-                              locationId,
-                              productType: "NWB",
+                                serviceProvider: "NBN",
+                                locationId,
+                                productType: "NWB",
                             }),
                             dataType: "json",
                             headers: {
-                              Accept: "application/json",
-                              "Content-Type": "application/json",
+                                Accept: "application/json",
+                                "Content-Type": "application/json",
                             },
                         });
-                      
+
                         qualify_service_request.done(function (data) {
-                          console.log(data);
-                          if (data.qualificationID && data.siteDetails) {
-                            if (data.siteDetails.nbnLocationID && data.accessQualificationList.length > 0) {
-                              var availableServices = data.accessQualificationList.filter(v => v.qualificationResult === 'PASS');
-                              if (availableServices.length > 0) {
-                                console.log(availableServices);
-                                setCookie("qualification_id", data.qualificationID);
-                                $("#row_qualify_success").removeClass("hidden");
-                                return;
-                              }
+                            console.log(data);
+                            if (data.qualificationID && data.siteDetails) {
+                                if (data.siteDetails.nbnLocationID && data.accessQualificationList.length > 0) {
+                                    var availableServices = data.accessQualificationList.filter(v => v.qualificationResult === 'PASS');
+                                    if (availableServices.length > 0) {
+                                        console.log(availableServices);
+                                        setCookie("qualification_id", data.qualificationID);
+                                        $("#row_qualify_success").removeClass("hidden");
+                                        return;
+                                    }
+                                }
                             }
-                          }
-                          $("#row_qualify_fail").removeClass("hidden");
+                            $("#row_qualify_fail").removeClass("hidden");
                         })
                         return;
                     }
@@ -247,8 +247,8 @@
             } else {
                 $("#row_qualify_fail").removeClass("hidden");
             }
-		});
-		
+        });
+
         /*request.done(function( data ) {
             $("#row_checking").addClass("hidden");
             if (data.status == "200") {
@@ -268,22 +268,22 @@
             $("#row_qualify_fail").removeClass("hidden");
             console.log( "Request failed: " + textStatus );
         });*/
-        
-        
+
+
     });
-    
-    $(".build-plan-button").click(function() {
+
+    $(".build-plan-button").click(function () {
         var next_section = $(".category-section:has(.product-item)");
         $('html, body').animate({
-            scrollTop: $(next_section).offset().top - 100  
+            scrollTop: $(next_section).offset().top - 100
         }, 1000);
     });
 
 
-	$( ".select-product-item" ).click(function() {
+    $(".select-product-item").click(function () {
         var product = $(this).parent();
         var category_idx = parseInt($(product).attr("data-no"));
-        
+
         if (category_idx < 4) {
             category_idx = 0;
         } else if (category_idx > 3 && category_idx < 7) {
@@ -301,7 +301,7 @@
         } else {
             var next_section = $(".order-summary-section");
         }
-        
+
         if ($(product).hasClass("selected")) {
             var product_id = $(product).attr("data-id");
             removeProductOnCart(product_id);
@@ -310,7 +310,7 @@
             $(this).text("Select");
         } else {
             var products = $(category).find(".product-item");
-            for (var i = 0; i < products.length; i ++) {
+            for (var i = 0; i < products.length; i++) {
                 if ($(products[i]).hasClass("selected")) {
                     var product_id = $(products[i]).attr("data-id");
                     removeProductOnCart(product_id);
@@ -327,12 +327,12 @@
             $(product).addClass("selected");
             $(this).text("Selected");
             $('html, body').animate({
-                scrollTop: $(next_section).offset().top - 100  
+                scrollTop: $(next_section).offset().top - 100
             }, 1000);
         }
     });
 
-    $(document).on( 'click', '.remove-order', function () {
+    $(document).on('click', '.remove-order', function () {
         var data_id = $(this).parent().attr("data-id");
         var data_no = parseFloat($(this).parent().attr("data-no"));
         var item = $(".product-item")[data_no];
@@ -341,7 +341,7 @@
 
         var price = $(item).find(".product-price");
         var sd = price.text().match(/[0-9.]+/g);
-        
+
         total_today -= parseFloat(sd);
         if (price.text().indexOf("mth") > 0) {
             total_monthly -= parseFloat(sd);
@@ -354,20 +354,22 @@
         displayTotalPrice();
     });
 
-    function owlCarouselHideUnusedArrow () {
-        const first = jQuery(`.dt-owl-item`).not(`.cloned`).eq(0)
-        const last = jQuery(`.dt-owl-item`).eq(first.index() - 1)
-        const left = jQuery(`.owl-nav > a`).eq(0)
-        const right = jQuery(`.owl-nav > a`).eq(1)
+    function owlCarouselHideUnusedArrow() {
+        const target = jQuery(`.carousel-shortcode-id-a86eaa763d34541d0981f58daee5fde8`)
+        const first = target.find(`.dt-owl-item`).not(`.cloned`).first()
+        const last = target.find(`.dt-owl-item`).not(`.cloned`).last()
+        const left = target.find(`.owl-nav > a`).eq(0)
+        const right = target.find(`.owl-nav > a`).eq(1)
 
-        if (!last.is(`.active`)) left.hide(), right.show()
-        else left.show(), right.hide()
+        target.find(`.owl-nav > a`).show()
+        if (first.is(`.active`)) left.hide()
+        else if (last.is(`.active`)) right.hide()
     }
 
-    $( document ).ready(function() {
+    $(document).ready(function () {
         loadProductsFromCart();
-        owlCarouselHideUnusedArrow()
-        jQuery(`.owl-nav > a`).click(owlCarouselHideUnusedArrow)
+        jQuery(`.carousel-shortcode-id-a86eaa763d34541d0981f58daee5fde8`).find(`.owl-nav > a`).eq(0).hide()
+        jQuery(`.carousel-shortcode-id-a86eaa763d34541d0981f58daee5fde8 .owl-nav > a`).click(owlCarouselHideUnusedArrow)
     });
-	
-})( jQuery );
+
+})(jQuery);
